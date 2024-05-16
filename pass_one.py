1eb0ecbe6c8686f672416f695a2efc5ca4702f29
Mhijazi16 <gultimate0@gmail.com>
2024-05-16 10:59:27 +0300

    pass_one : writing pass_one() method for pass_one algorithm

--- pass_one.py
+++ pass_one.py
@@ -43,3 +43,47 @@ def write_headers():
     LOCCTR = one[2]
     SYMTAB[one[1]] = one[2]
     sym.write(f"{one[1]} {one[2]} \n")
+
+def pass_one():
+    global LOCCTR
+    index = 1
+    for line in code:
+        i = 0 
+        n = line.strip().split()
+        
+        if not n : 
+            continue
+        if n[i] !='.':
+
+            intmdt.write(f"{index}          ")
+            intmdt.write(hex(int(LOCCTR,16)) + "      ")
+            intmdt.write("".join(line))
+            
+            if n[i] not in OPTAB.keys() and n[i] != '-':
+                current = hex(int(LOCCTR,16))
+                SYMTAB[n[i]] = str(current)
+                sym.write(f"{n[i]} {current} \n")
+                i += 1
+
+
+            if n[i] in OPTAB.keys() or n[i]=="WORD":
+                current = hex(int(LOCCTR,16)+3)
+                LOCCTR = str(current)
+            elif n[i]=="BYTE":
+                if n[i+1][0]=="X":
+                    size = int((len(n[i+1])-3)/2)
+                    current = hex(int(LOCCTR,16)+size)
+                    LOCCTR = str(current)
+                elif n[i+1][0]=="C":
+                    size = (len(n[i+1])-3)
+                    current = hex(int(LOCCTR,16)+size)
+                    LOCCTR = str(current)
+            elif n[i]=="RESW":
+                words = int(n[i+1],16) * 3 
+                current = hex(int(LOCCTR,16)+words)
+                LOCCTR = str(current)
+            elif n[i]=="RESB":
+                size = int(n[i+1])
+                current = hex(int(LOCCTR,16)+size)
+                LOCCTR = str(current)
+            index += 1
