c9142b7c6d5bb0839eb7bdf9e6843ebbdabf6720
Mhijazi16 <gultimate0@gmail.com>
2024-05-19 20:56:29 +0300

    pass_two : definting essential datastructures

---
+++ pass_two.py
@@ -0,0 +1,9 @@
+# Made By Mohammed Hijazi
+from typing import Dict, List
+import sys 
+
+OPTAB: Dict[str,str] = {}
+SYMTAB: Dict[str,str] = {}
+DIREC = ["RSUB","WORD","BYTE","RESW","RESB"]
+PRGNAME = ""
+CODE = []
